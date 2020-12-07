57c701383576bacf387816f479ccc5cf067cabc7
Susan Luke <susan_luke@mail.com>
2020-12-07 15:23:02 +0000

    Added Part 2

--- src/advent_of_code/day_four.clj
+++ src/advent_of_code/day_four.clj
@@ -4,6 +4,13 @@
 
 (def f "day4-input.txt")
 
+(defn parse-file []
+  (->> (-> (io/resource f)
+           slurp
+           (string/split #"\n\n"))
+       (map #(string/split %1 #"\n|\s"))
+       (map data->passport-map)))
+
 (defn data->passport-map [v]
   (->> v
        (map (partial re-matches #"^(\w*):([\w#]*)$"))
@@ -16,12 +23,21 @@
           ;; exclude :cid
           [:byr :iyr :eyr :hgt :hcl :ecl :pid]))
 
-(defn parse-file []
-  (->> (-> (io/resource f)
-           slurp
-           (string/split #"\n\n"))
-       (map #(string/split %1 #"\n|\s"))
-       (map data->passport-map)))
+(defn valid-passport-pt2? [{:keys [byr iyr eyr hgt hcl ecl pid]}]
+  (let [byr (read-string byr)
+        iyr (read-string iyr)
+        eyr (read-string eyr)
+        [_ h-num-raw h-unit] (re-matches #"^(\d+)(cm|in)$" hgt)
+        h-num (and h-num-raw (read-string h-num-raw))]
+    (and (integer? byr) (<= 1920 byr 2002)
+         (integer? iyr) (<= 2010 iyr 2020)
+         (integer? eyr) (<= 2020 eyr 2030)
+         (integer? h-num) (#{"cm" "in"} h-unit) (if (= "cm" h-unit)
+                                                  (<= 150 h-num 193)
+                                                  (<= 59 h-num 76))
+         (re-matches #"^#[0-9a-f]{6}" hcl)
+         (#{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} ecl)
+         (re-matches #"[0-9]{9}" pid))))
 
 (defn get-day4-answer-pt1
   []
@@ -32,6 +48,6 @@
 (defn get-day4-answer-pt2
   []
   (->> (parse-file)
-       ;; add filter
-       count
-       ))
+       (filter valid-passport-pt1?)
+       (filter valid-passport-pt2?)
+       count))
--- test/advent_of_code/day_four_test.clj
+++ test/advent_of_code/day_four_test.clj
@@ -4,4 +4,5 @@
 
 (deftest get-day4-answer-test
   (testing "Test Day 4"
-    (is (= 200 (sut/get-day4-answer-pt1)))))
+    (is (= 200 (sut/get-day4-answer-pt1)))
+    (is (= 116 (sut/get-day4-answer-pt2)))))
