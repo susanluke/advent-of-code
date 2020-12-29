044a1ade4cc7bc2c77cb86611be88805b6f8b051
Susan Luke <susan_luke@mail.com>
2020-12-29 22:28:11 +0000

    Not quite working

--- src/advent_of_code/day_fourteen.clj
+++ src/advent_of_code/day_fourteen.clj
@@ -7,35 +7,75 @@
 (def mask-re #"mask = ([10X]+)")
 (def mem-re  #"mem\[([0-9]+)\] = ([0-9]+)")
 
-(defn s->mask [s x-replacement]
+(defn s->mask
+  "version 1, interprets Xs to be ignored"
+  [s x-replacement]
   (read-string (str "2r" (string/replace s #"X" x-replacement))))
 
-(defn s->instruction [s]
+(defn append-char-to-all-elements [l c]
+  (map #(str % c) l))
+
+(defn s->masks
+  "version 2, interprets X as either 1 or 0"
+  [s]
+  (->> s
+       (reduce (fn [l c]
+                 (if (or (= \0 c) (= \1 c))
+                   (append-char-to-all-elements l c)
+                   (concat (append-char-to-all-elements l 0)
+                           (append-char-to-all-elements l 1))))
+               [""])
+       (map #(read-string (str "2r" %)))))
+
+(defn s->instruction [v s]
   (if-let [[_ mask] (re-matches mask-re s)]
-    [:mask {:and-mask (s->mask mask "1")
-            :or-mask  (s->mask mask "0")}]
+    [:mask (if (= :v1 v)
+             {:and-mask (s->mask mask "1")
+              :or-mask  (s->mask mask "0")}
+             (s->masks mask))]
     (if-let [[_ mem-loc val] (re-matches mem-re s)]
       [:mem (read-string mem-loc) (read-string val)])))
 
-(defn parse-string [s]
+(defn parse-string [v s]
   (->> (string/split-lines s)
-       (map s->instruction)))
+       (map (partial s->instruction v))))
 
 (defn apply-mask [{:keys [and-mask or-mask]} v]
   (-> v (bit-and and-mask) (bit-or or-mask)))
 
-(defn perform-instruction [{:keys [mask] :as state} [cmd :as instruction]]
+(defn perform-instruction-v1 [{:keys [mask] :as state} [cmd :as instruction]]
   (case cmd
     :mask (assoc state :mask (second instruction))
     :mem  (let [[l v] (rest instruction)]
             (assoc-in state [:memory l] (apply-mask mask v)))))
 
-(defn perform-instructions [instructions]
+(defn perform-instruction-v2 [{:keys [mask] :as state} [cmd :as instruction]]
+  (println "intermediate state:" state)
+  (case cmd
+    :mask (assoc state :mask (second instruction))
+    :mem (let [[l v] (rest instruction)]
+           (reduce #(assoc-in %1 [:memory (bit-or %2 l)] v)
+                   state
+                   mask))))
+
+(defn perform-instructions [v instructions]
+  (println (reduce (if (= :v1 v)
+                     perform-instruction-v1
+                     perform-instruction-v2)
+                   {}
+                   instructions))
   (->> instructions
-       (reduce perform-instruction {})
+       (reduce (if (= :v1 v)
+                 perform-instruction-v1
+                 perform-instruction-v2)
+               {})
        :memory
        vals
        (apply +)))
 
 (defn get-day14-answer-pt1 []
-  (perform-instructions (parse-string input)))
+  (perform-instructions :v1 (parse-string :v1 input)))
+
+;; 1756854330635 is too low
+(defn get-day14-answer-pt2 []
+  (perform-instructions :v2 (parse-string :v2 input)))
--- test/advent_of_code/day_fourteen_test.clj
+++ test/advent_of_code/day_fourteen_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [advent-of-code.day-fourteen :as sut]))
 
+(def test-input
+  "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0")
+
+(def test-input2
+  "mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1")
+
 (deftest get-day14-answer-test
   (testing "Test Day 14"
-    (is (= 11501064782628 (sut/get-day14-answer-pt1)))))
+    (is (= 11501064782628 (sut/get-day14-answer-pt1)))
+    (is (= 165 (sut/perform-instructions :v1 (sut/parse-string :v1 test-input))))
+    (is (= 208 (sut/perform-instructions :v2 (sut/parse-string :v2 test-input2))))
+    ))
